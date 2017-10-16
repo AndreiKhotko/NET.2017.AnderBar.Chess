e88b5a3c1a6e0c896c91d6ca7cdab694600b6e94
AndreiKhotko <khotko.epam@gmail.com>
2017-10-16 18:36:46 +0300

    add BusyCellException

---
+++ ChessEngine/Exceptions/BusyCellException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessEngine.Exceptions
+{
+    public class BusyCellException : Exception
+    {
+        public BusyCellException() { }
+
+        public BusyCellException(string message) : base(message) { }
+
+        public BusyCellException(string message, Exception inner) : base(message, inner) { }
+
+        protected BusyCellException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
