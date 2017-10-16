6b3e9e86b6d9fe95b44f5a029614042487408bdf
AndreiKhotko <khotko.epam@gmail.com>
2017-10-16 18:37:27 +0300

    add Pieces

---
+++ ChessEngine/Pieces/Bishop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessEngine.BaseTypes;
+
+namespace ChessEngine.Pieces
+{
+    public class Bishop : Piece
+    {
+        public Bishop(ChessPoint coordinate, Color color) : base(coordinate, color)
+        {
+            
+        }
+    }
+}
---
+++ ChessEngine/Pieces/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessEngine.BaseTypes;
+
+namespace ChessEngine.Pieces
+{
+    public abstract class Piece
+    {
+        public ChessPoint Coordinate { get; set; }
+
+        public Color Color { get; }
+
+        protected Piece(ChessPoint coordinate, Color color)
+        {
+            Coordinate = coordinate;
+            Color = color;
+        }
+    }
+}
